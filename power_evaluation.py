57d5a99fcc478a157fe696d92577259c5766df09
malwash <malwash@gmail.com>
2022-10-25 15:58:02 +0200

    Added futher Enmix annotation

--- power_evaluation.py
+++ power_evaluation.py
@@ -160,9 +160,10 @@ def limma_test(simulated_df, n_group1, n_group2):
 
 def logit_transformation(beta_matrix):
     robjects.r('''
+                chooseCRANmirror(ind = 1)
                 if (!requireNamespace("BiocManager", quietly = TRUE))
-                if (!requireNamespace("ENmix", quietly = TRUE))
                 BiocManager::install("ENmix", update=True)
+                install.packages('ENmix', repos='https://cloud.r-project.org/')
                 library(ENmix)
                 ENmix_transform <- function(beta_matrix, verbose=FALSE) {
                 m_matrix = as.data.frame(B2M(beta_matrix))
